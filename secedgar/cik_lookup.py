495c688cded04324167c4563289e85ff4ead6bce
Niklas E <niklaseidt98@gmail.com>
2022-01-04 18:07:52 +0100

    FIX: replace null with empty string instead of skip object (#261)

--- secedgar/cik_lookup.py
+++ secedgar/cik_lookup.py
@@ -31,8 +31,7 @@ def get_cik_map():
     """
     response = requests.get("https://www.sec.gov/files/company_tickers.json")
     json_response = response.json()
-    return {key: {v[key].upper(): str(v["cik_str"]) for v in json_response.values()
-                  if v[key] is not None}
+    return {key: {(v[key].upper() if v[key] is not None else ""): str(v["cik_str"]) for v in json_response.values()}
             for key in ("ticker", "title")}
 
 
